e2def25fe93c40deaefbb0a632479b15f47d65e4
lwj <2780307702@qq.com>
2026-03-23 18:14:40 +0800

    feat: 添加SellPointPage页面基础结构

---
+++ scripts/pages/SellPointPage.js
@@ -0,0 +1,9 @@
+export class SellPointPage extends IPage {
+    constructor(slide, data = {}, options = {}) {
+        super(slide, data, options);
+    }
+
+    render() {
+        throw new Error("Method not implemented.");
+    }
+}
