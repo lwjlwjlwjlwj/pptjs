7f2ac5104872ea913a5e3719f531ab00cba4d8a1
lwj <2780307702@qq.com>
2026-03-25 16:31:06 +0800

    feat(SellPointPage): 实现文本渲染功能并添加颜色配置
    添加文本数组和pushText方法用于存储文本数据，在render方法中实现文本渲染功能。从配置文件中引入颜色常量，支持自定义文本颜色。

--- scripts/pages/SellPointPage.js
+++ scripts/pages/SellPointPage.js
@@ -1,11 +1,41 @@
 import { IPage } from "./IPage.js";
+import { COLORS } from "../config.js";
 
 export class SellPointPage extends IPage {
     constructor(slide, data = {}, options = {}) {
         super(slide, data, options);
+        this.textArr = []; // 文本数组
+    }
+
+    // push 文本
+    pushText(text, color = COLORS.BLACK) {
+        const item = {
+            text: text,
+            options: {
+                color: color,
+            }
+        }
+        this.textArr.push(item);
     }
 
     render() {
-        console.log("test");
+        // 循环读取data，添加到textArr中
+        for (let key in this.data) {
+            this.pushText(this.data[key]);
+        }
+
+        // 渲染文本
+        this.slide.addText(this.textArr, {
+            x: 0,
+            y: 0,
+            w: LAYOUT_WIDTH,
+            h: LAYOUT_HEIGHT,
+            align: "left",
+            valign: "middle",
+            fontSize: bestFontSize,
+            bold: true,
+            lineSpacing: bestFontSize * 1.5,
+            fontFace: "微软雅黑",
+        })
     }
 }
